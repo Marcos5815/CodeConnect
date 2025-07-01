842be38cb27726b0261fb1df663129037bb8021d
Marcos Silva <marcos5815mvs@gmail.com>
2025-07-01 20:06:49 -0300

    estiliza a seção principal e adiciona funcionalidade ao botão de carregar imagem

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.querySelector("#upload__btn");
+const inputUpload = document.querySelector("#image__upload");
+
+uploadBtn.addEventListener("click", (evento) => {
+    inputUpload.click();
+})
